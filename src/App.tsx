71f78e4c668bced13494d45b2aa5b7e473219c04
MrPattarapong_Phengtavee <phattarapong.phe@spumail.net>
2025-04-19 01:24:50 +0700

    เปลี่ยนจาก BrowserRouter เป็น HashRouter สำหรับ GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Marketplace from "./pages/Marketplace";
@@ -28,7 +28,7 @@ const App = () => (
         <ContractProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/marketplace" element={<Marketplace />} />
@@ -45,7 +45,7 @@ const App = () => (
               <Route path="/user-orders" element={<UserOrders />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </ContractProvider>
       </Web3Provider>
     </TooltipProvider>
